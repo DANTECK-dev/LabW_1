bc028f5256af0220c9b402ac2992ad15b51e5ec4
SAS <SAS@DESKTOP-VQUL9MS>
2021-10-06 23:44:17 +0800

    Добавьте файлы проекта.

---
+++ LabW_1/Source1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "¬ведите кол-во продуктов";
+	const int col;
+	cin >> col;
+	//int* arr_c = new int[col];
+	int arr_c[col]{};
+}
---
+++ LabW_1/Source2.cpp
@@ -0,0 +1,25 @@
+/*#include <iostream>
+
+int main() {
+
+    size_t size;
+
+    std::cout << "Please, enter a size of the array: ";
+    std::cin >> size;
+
+    int* array = new int[size];
+
+    std::cout << "Please, enter " << size << " values: ";
+
+    for (size_t i = 0; i < size; i++) {
+
+        std::cin >> array[i];
+
+    }
+
+    delete[] array;
+
+    std::system("PAUSE");
+    return EXIT_SUCCESS;
+
+}*/
